94fd8b2f1cf38564d22aaa1de7bd8d2b490f6cc3
Mike <mike.lin@dxc.com>
2023-02-06 12:25:35 +0800

    新增 postForEntity & getForEntity 實例

--- cloud-consumer-order80/src/main/java/per/mike/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/per/mike/springcloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package per.mike.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,12 +26,34 @@ public class OrderController {
   @Autowired private RestTemplate restTemplate;
 
   @GetMapping("/create")
-  public CommonResult<Integer> create(@RequestBody Payment payment) {
+  public CommonResult create(@RequestBody Payment payment) {
     return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
   }
 
   @GetMapping("/get/{id}")
   public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
     return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+  }
+
+  @GetMapping("/createForEntity")
+  public CommonResult createForEntity(@RequestBody Payment payment) {
+    ResponseEntity<CommonResult> res =
+        restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    if (res.getStatusCode().is2xxSuccessful()) {
+      return res.getBody();
+    } else {
+      return new CommonResult<Integer>(500, "操作失敗");
+    }
+  }
+
+  @GetMapping("/getForEntity/{id}")
+  public CommonResult getPaymentForEntity(@PathVariable("id") Long id) {
+    ResponseEntity<CommonResult> res =
+        restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    if (res.getStatusCode().is2xxSuccessful()) {
+      return res.getBody();
+    } else {
+      return new CommonResult<Payment>(500, "操作失敗");
+    }
   }
 }
