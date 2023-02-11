3db95f3a9f532ced63d6dcc3c337cc9fd90d4ac4
Mike <mike.lin@dxc.com>
2023-02-11 16:38:42 +0800

    add stream consumer and set group

---
+++ cloud-stream-consumer8803/src/main/java/per/mike/springcloud/controller/ConsumerController.java
@@ -0,0 +1,21 @@
+package per.mike.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Sink.class)
+public class ConsumerController {
+
+	@Value("${server.port}")
+	private String serverPort;
+
+	@StreamListener(Sink.INPUT)
+	public void input(Message<String> message) {
+		System.out.println("消費者2, ****** 接收訊息: " + message.getPayload() + " ==== Server Port: " + serverPort);
+	}
+}
