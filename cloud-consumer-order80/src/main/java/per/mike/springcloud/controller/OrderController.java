d2cfb101e132dd27e7d755ac4a5d405e5654a376
Mike <mike.lin@dxc.com>
2023-02-12 01:56:48 +0800

    add zipkin and sleuth

--- cloud-consumer-order80/src/main/java/per/mike/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/per/mike/springcloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package per.mike.springcloud.controller;
 
 import java.net.URI;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 import per.mike.springcloud.entities.CommonResult;
 import per.mike.springcloud.entities.Payment;
 import per.mike.springcloud.loadbalance.myLb;
@@ -25,57 +27,66 @@ import per.mike.springcloud.loadbalance.myLb;
 @RequestMapping("/order/consumer/payment")
 public class OrderController {
 
-  // private static final String PAYMENT_URL = "http://localhost:8001";
-  private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+	private static final String PAYMENT_URL = "http://localhost:8001";
+//	private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
-  @Autowired private RestTemplate restTemplate;
+	@Autowired
+	private RestTemplate restTemplate;
 
-  @Autowired private myLb myLb;
+	@Autowired
+	private myLb myLb;
 
-  @Autowired private DiscoveryClient discoveryClient;
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
-  @GetMapping("/create")
-  public CommonResult create(@RequestBody Payment payment) {
-    return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
-  }
+	@GetMapping("/create")
+	public CommonResult create(@RequestBody Payment payment) {
+		return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+	}
 
-  @GetMapping("/get/{id}")
-  public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
-    return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
-  }
+	@GetMapping("/get/{id}")
+	public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+		return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+	}
 
-  @GetMapping("/createForEntity")
-  public CommonResult createForEntity(@RequestBody Payment payment) {
-    ResponseEntity<CommonResult> res =
-        restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
-    if (res.getStatusCode().is2xxSuccessful()) {
-      return res.getBody();
-    } else {
-      return new CommonResult<Integer>(500, "操作失敗");
-    }
-  }
+	@GetMapping("/createForEntity")
+	public CommonResult createForEntity(@RequestBody Payment payment) {
+		ResponseEntity<CommonResult> res = restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment,
+				CommonResult.class);
+		if (res.getStatusCode().is2xxSuccessful()) {
+			return res.getBody();
+		} else {
+			return new CommonResult<Integer>(500, "操作失敗");
+		}
+	}
 
-  @GetMapping("/getForEntity/{id}")
-  public CommonResult getPaymentForEntity(@PathVariable("id") Long id) {
-    ResponseEntity<CommonResult> res =
-        restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
-    if (res.getStatusCode().is2xxSuccessful()) {
-      return res.getBody();
-    } else {
-      return new CommonResult<Payment>(500, "操作失敗");
-    }
-  }
+	@GetMapping("/getForEntity/{id}")
+	public CommonResult getPaymentForEntity(@PathVariable("id") Long id) {
+		ResponseEntity<CommonResult> res = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id,
+				CommonResult.class);
+		if (res.getStatusCode().is2xxSuccessful()) {
+			return res.getBody();
+		} else {
+			return new CommonResult<Payment>(500, "操作失敗");
+		}
+	}
 
-  @GetMapping("lb")
-  public CommonResult getPaymentLb() {
-    List<ServiceInstance> serviceInstances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-    if (serviceInstances == null || serviceInstances.size() <= 0) {
-      return new CommonResult<String>(500, "調用失敗 for LB", null);
-    }
+	@GetMapping("lb")
+	public CommonResult getPaymentLb() {
+		List<ServiceInstance> serviceInstances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+		if (serviceInstances == null || serviceInstances.size() <= 0) {
+			return new CommonResult<String>(500, "調用失敗 for LB", null);
+		}
 
-    ServiceInstance serviceInstance = myLb.instances(serviceInstances);
-    URI uri = serviceInstance.getUri();
+		ServiceInstance serviceInstance = myLb.instances(serviceInstances);
+		URI uri = serviceInstance.getUri();
 
-    return restTemplate.getForObject(uri + "/payment/lb", CommonResult.class);
-  }
+		return restTemplate.getForObject(uri + "/payment/lb", CommonResult.class);
+	}
+
+	@GetMapping("/zipkin")
+	public CommonResult zipkin() {
+		CommonResult res = restTemplate.getForObject(PAYMENT_URL + "/payment/zipkin", CommonResult.class);
+		return res;
+	}
 }
--- cloud-provider-payment8001/src/main/java/per/mike/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/per/mike/springcloud/controller/PaymentController.java
@@ -1,7 +1,7 @@
 package per.mike.springcloud.controller;
 
 import java.util.concurrent.TimeUnit;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 import per.mike.springcloud.entities.CommonResult;
 import per.mike.springcloud.entities.Payment;
 import per.mike.springcloud.service.PaymentService;
@@ -26,65 +28,67 @@ import per.mike.springcloud.service.PaymentService;
 @RequestMapping(value = "/payment")
 public class PaymentController {
 
-  //  @Autowired private DiscoveryClient discoveryClient;
-  @Autowired private DiscoveryClient discoveryClient;
-  @Autowired private PaymentService paymentService;
+	// @Autowired private DiscoveryClient discoveryClient;
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	@Autowired
+	private PaymentService paymentService;
 
-  @Value("${server.port}")
-  private String serverPort;
+	@Value("${server.port}")
+	private String serverPort;
 
-  @PostMapping(value = "/create")
-  public CommonResult<Integer> create(@RequestBody Payment payment) {
-    int result = paymentService.create(payment);
-    if (result > 0) {
-      return new CommonResult<Integer>(200, "新增成功,serverPort: " + serverPort, result);
-    } else {
-      return new CommonResult<Integer>(500, "新增失敗,serverPort: " + serverPort, 0);
-    }
-  }
+	@PostMapping(value = "/create")
+	public CommonResult<Integer> create(@RequestBody Payment payment) {
+		int result = paymentService.create(payment);
+		if (result > 0) {
+			return new CommonResult<Integer>(200, "新增成功,serverPort: " + serverPort, result);
+		} else {
+			return new CommonResult<Integer>(500, "新增失敗,serverPort: " + serverPort, 0);
+		}
+	}
 
-  @GetMapping(value = "/get/{id}")
-  public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
-    Payment result = paymentService.getPaymentById(id);
-    if (result != null) {
-      return new CommonResult<Payment>(200, "查詢成功,serverPort: " + serverPort, result);
-    } else {
-      return new CommonResult<Payment>(500, "查詢無資料,serverPort: " + serverPort, null);
-    }
-  }
+	@GetMapping(value = "/get/{id}")
+	public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+		Payment result = paymentService.getPaymentById(id);
+		if (result != null) {
+			return new CommonResult<Payment>(200, "查詢成功,serverPort: " + serverPort, result);
+		} else {
+			return new CommonResult<Payment>(500, "查詢無資料,serverPort: " + serverPort, null);
+		}
+	}
 
-  @GetMapping(value = "/discovery")
-  public CommonResult<Object> getDiscovery() {
+	@GetMapping(value = "/discovery")
+	public CommonResult<Object> getDiscovery() {
 
-    for (String svc : discoveryClient.getServices()) {
-      log.info("******" + String.join("\t", svc));
+		for (String svc : discoveryClient.getServices()) {
+			log.info("******" + String.join("\t", svc));
 
-      for (ServiceInstance ins : discoveryClient.getInstances(svc)) {
-        log.info(
-            String.join(
-                "\t",
-                ins.getServiceId(),
-                ins.getHost(),
-                String.valueOf(ins.getPort()),
-                ins.getUri().toString()));
-      }
-    }
+			for (ServiceInstance ins : discoveryClient.getInstances(svc)) {
+				log.info(String.join("\t", ins.getServiceId(), ins.getHost(), String.valueOf(ins.getPort()),
+						ins.getUri().toString()));
+			}
+		}
 
-    return new CommonResult<Object>(200, "查詢成功", discoveryClient);
-  }
+		return new CommonResult<Object>(200, "查詢成功", discoveryClient);
+	}
 
-  @GetMapping(value = "/lb")
-  public CommonResult<String> getLb() {
-    return new CommonResult<String>(200, "調用成功 for LB", serverPort);
-  }
+	@GetMapping(value = "/lb")
+	public CommonResult<String> getLb() {
+		return new CommonResult<String>(200, "調用成功 for LB", serverPort);
+	}
 
-  @GetMapping(value = "/openfeign/timeout")
-  public CommonResult<String> getOpenfeignTimeout() {
-    try {
-      TimeUnit.SECONDS.sleep(3);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-    return new CommonResult<String>(200, "調用成功 for openfeign timeout", serverPort);
-  }
+	@GetMapping(value = "/openfeign/timeout")
+	public CommonResult<String> getOpenfeignTimeout() {
+		try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return new CommonResult<String>(200, "調用成功 for openfeign timeout", serverPort);
+	}
+
+	@GetMapping(value = "/zipkin")
+	public CommonResult<String> zipkin() {
+		return new CommonResult<String>(200, "調用成功 for Zipkin", serverPort);
+	}
 }
